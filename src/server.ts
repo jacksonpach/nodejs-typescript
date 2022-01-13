5fee1a3f3db83777cff8e75558ce1384b6e596f4
jack <jackpa@live.com>
2022-01-12 21:29:34 -0300

    fix(server): Fix Server Class

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,22 @@ import express from 'express'
 import Routes from "./routes";
 
 class Server {
+    app: any //@todo implement type
+
     constructor() {
-        const app = express();
-
-        app.use(express.json())
-        app.use(Routes.router)
-
-        app.listen(3333, () => 'server running on http://localhost:3333')
+        this.app = express()
+        this.createApp()
+        this.initialApp()
     }
 
-    cr
+    createApp() {
+        this.app.use(express.json())
+        this.app.use(Routes.router)
+    }
+
+    initialApp() {
+        this.app.listen(3333, () => 'server running on http://localhost:3333')
+    }
 }
+
 new Server();
